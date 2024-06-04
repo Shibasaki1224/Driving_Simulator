27cc51f4aec9b76d70eae212809428fcc9ca9a37
Shibasaki1224 <122085673+Shibasaki1224@users.noreply.github.com>
2024-06-04 15:40:42 +0900

    Delete C# directory

--- C#/dust/Rail_Select.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Cinemachine;
-
-public class Rail_Select : MonoBehaviour
-{
-    public CinemachinePathBase[] Rail;
-}
